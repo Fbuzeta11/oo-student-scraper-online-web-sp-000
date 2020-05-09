a49fdd34ceb1bd651ac328f59f73052744e3fa60
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-05-09 18:18:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,13 +27,13 @@ class Scraper
     container = page.css(".social-icon-container a").collect{|icon| icon.attribute("href").value}
     container.each do |link|
       if link.include?("twitter")
-        student[:twitter] = link
+        students[:twitter] = link
       elsif link.include?("linkedin")
-        student[:linkedin] = link
+        students[:linkedin] = link
       elsif link.include?("github")
-        student[:github] = link
+        students[:github] = link
       else link.include?(".com")
-        student[:blog] = link
+        students[:blog] = link
       end
     end
     students << link
